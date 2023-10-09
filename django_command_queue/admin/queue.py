d532a08d3e8b735e869b30df0a8cef6f8ee64312
andrewp-as-is <andrewp-as-is@users.noreply.github.com>
2023-10-09 11:57:45 +0300

    -

---
+++ django_command_queue/admin/queue.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from ..models import Queue
+
+class QueueAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+    ]
+
+admin.site.register(Queue,QueueAdmin)
---
+++ django_command_queue/management/commands/command_queue.py
@@ -0,0 +1,13 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+from ...models import Queue
+
+class Command(BaseCommand):
+
+    def handle(self,*args,**options):
+        for queue in Queue.objects.all():
+            try:
+                call_command(queue.name)
+            finally:
+                Queue.objects.filter(id=queue.id).delete()
